4454dd351dad798a24fd5f06f0b6c6376c24021d
marcojkeyzer <166999070+marcojkeyzer@users.noreply.github.com>
2025-11-06 19:03:13 +0000

    profile icon tab rename

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { createTrailbaseProvider } from "../ra-trailbase"
 import { CategoriesCreate, CategoriesEdit, CategoriesList, CategoriesShow } from "./categories"
 import { ProductCreate, ProductEdit, ProductList, ProductShow } from "./products"
 import { ProfilesEdit, ProfilesList } from "./profiles"
+import PeopleIcon from '@mui/icons-material/People';
 
 const {dataProvider,authProvider} = await createTrailbaseProvider("https://urban-invention-wrvqq597j4rjcg7pv-4000.app.github.dev/")
 
@@ -10,9 +11,9 @@ function App() {
 
   return (
     <Admin dataProvider={dataProvider} authProvider={authProvider}>
+      <Resource name="profiles" list={ProfilesList} edit={ProfilesEdit} options={{ label: 'My Profile' }} icon={PeopleIcon}/>
       <Resource name="categories" list={CategoriesList} show={CategoriesShow} edit={CategoriesEdit} create={CategoriesCreate} />
       <Resource name="products" list={ProductList} show={ProductShow} edit={ProductEdit} create={ProductCreate} />
-      <Resource name="profiles" list={ProfilesList} edit={ProfilesEdit}/>
       <Resource name="users"/>
     </Admin>
   )
